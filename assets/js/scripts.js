0a401d900f401419ac68618e80d03ab03dee9413
Danilo <fdgrijalbac@gmail.com>
2026-01-19 16:53:15 -0500

    Refactor login.html for improved structure and update temp.html to implement category selection with dynamic content display

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+//? NAVBAR START
 const menuToggle = document.getElementById('openMenu');
 const navbar = document.querySelector('.navbar-mobile-container');
 const navbarLinks = document.querySelectorAll('.navbar-links a');
@@ -17,8 +18,45 @@ navbarLinks.forEach(link => {
         navbar.classList.remove('active');
     })
 });
+//? NAVBAR END
 
 
+//? CATEGORIES SELECT START
+const categoryItems = document.querySelectorAll('.categories li');
+const categoryContents = document.querySelectorAll('.categories-content');
+
+categoryItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        // Quitar active de todas las categorías (li)
+        categoryItems.forEach(li => li.classList.remove('active'));
+
+        // Agregar active a la seleccionada
+        item.classList.add('active');
+
+        // Obtener el nombre de la categoría
+        const categoryName = item.textContent.trim().toLowerCase();
+
+        // Ocultar todos los contenidos
+        categoryContents.forEach(content => {
+            content.classList.remove('active');
+        });
+
+        // Mostrar el contenido correspondiente
+        const activeContent = document.querySelector(
+            `.categories-content.${categoryName}`
+        );
+
+        if (activeContent) {
+            activeContent.classList.add('active');
+        }
+    });
+});
+//? CATEGORIES SELECT END 
+
+
+//? CLIENTS SAY START
 const total = 5;
 
 const containers = document.querySelectorAll('.stars-rating');
@@ -26,3 +64,5 @@ const containers = document.querySelectorAll('.stars-rating');
 containers.forEach(container => {
     container.innerHTML = '<i class="fa-solid fa-star"></i>'.repeat(total);
 });
+//? CLIENTS SAY END 
+
